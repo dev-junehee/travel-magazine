a0ba44e2d3989c81ff45f475b9443b1ba93bd60f
dev-junehee <dev.junehee@gmail.com>
2024-05-26 20:36:07 +0900

    Initial Commit

---
+++ travel-magazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travel-magazine
+//
+//  Created by junehee on 5/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
