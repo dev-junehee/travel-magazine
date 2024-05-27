d1bd7674638e9ffbf1325dffef0052377e3a6708
dev-junehee <dev.junehee@gmail.com>
2024-05-27 21:44:47 +0900

    Feat: 도시 정보 탭 추가

---
+++ travel-magazine/Controller/CityInfoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CityInfoViewController.swift
+//  travel-magazine
+//
+//  Created by junehee on 5/27/24.
+//
+
+import UIKit
+
+class CityInfoViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
