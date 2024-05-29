ead621228f7636eba35b204d25ae71b00a50e5c4
dev-junehee <dev.junehee@gmail.com>
2024-05-29 22:05:27 +0900

    Feat: 관광지 셀 - 디테일 화면 전환 추가 (Push-Pop)

--- travel-magazine/Controller/CityInfoDetailViewController.swift
+++ travel-magazine/Controller/CityInfoDetailViewController.swift
@@ -12,6 +12,17 @@ class CityInfoDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewTitle("관광지 화면")
+        setPopBarButton()
     }
 
+    func setPopBarButton() {
+        let popImage = UIImage(systemName: "chevron.left")
+        let popButton = UIBarButtonItem(title: nil, image: popImage, target: self, action: #selector(popBarButtonClicked))
+        navigationItem.leftBarButtonItem = popButton
+        navigationItem.leftBarButtonItem?.tintColor = .black
+    }
+    
+    @objc func popBarButtonClicked() {
+        navigationController?.popViewController(animated: true)
+    }
 }
